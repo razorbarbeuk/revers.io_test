07f9b4ab1f98d21273bf1a484b116072570a58e1
razorbarbeuk <bourson.g@gmail.com>
2022-03-28 09:48:31 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./styles.css";
+import { useState, useEffect } from "react"
+import "./styles.css"
 
 // https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port
 
@@ -11,22 +12,66 @@ import "./styles.css";
 // bonus: use an input to research an other query
 
 export default function App() {
+  const [loading, setLoading] = useState(false)
+  const [list, setList] = useState(null)
+  const [error, setError] = useState(null)
+  const url = new URL('https://api-adresse.data.gouv.fr/search/?q=8+bd+du+port')
+  const query = url.searchParams.get('q')
+
+  const fetch_address = async (url) => {
+    setLoading(true)
+    await fetch(url, {
+      method: "GET"
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        }
+        return res
+      })
+      .then((data) => setList(data))
+      .catch((err) => setError(err))
+      .finally((_) => setLoading(false))
+  }
+
+  const compute_score = (number) => `${Math.round(number*100)} %`
+
+  useEffect(() => {
+    fetch_address(url)
+  }, [])
+
+  if (loading) {
+    return <>loading...</>
+  }
+  if(error) {
+    return <>error</>
+  }
+
   return (
     <div className="App">
       <h1>Addresse list</h1>
       <ul>
         <li>
-          <h2>Label: x, xxxxx xxxx xxxxxxx</h2>
+          {url &&
+            <h2>Label: {query}</h2>
+          }
           <ul>
-            <li>number: x</li>
-            <li>street: xxx</li>
-            <li>city: xxx</li>
-            <li>zipcode: xxxxx</li>
-            <li>country: France</li>
-            <li>rounded score: x.xx</li>
+            {list &&
+              list.features.map((address) => {
+                return (
+                  <div key={address.properties.label} style={{ marginBottom: "10px" }}>
+                    <li>number: {address.properties.housenumber}</li>
+                    <li>street: {address.properties.street}</li>
+                    <li>city: {address.properties.city}</li>
+                    <li>zipcode: {address.properties.citycode}</li>
+                    <li>country: France</li>
+                    <li>rounded score: {compute_score(address.properties.score)}</li>
+                  </div>
+                )
+              })}
           </ul>
         </li>
       </ul>
     </div>
-  );
+  )
 }
